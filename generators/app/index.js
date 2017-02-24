437ab4feffc13429800b6922e52512b2fe4ce264
Leandro Segovia <ldlsegovia@gmail.com>
2017-02-24 11:45:12 -0300

    feat(): add README and delete jshint and editor config files

--- generators/app/index.js
+++ generators/app/index.js
@@ -3,6 +3,13 @@ const Generator = require('yeoman-generator');
 const process = require('process');
 const exec = require('child_process').execSync;
 
+const folderName = process.cwd().split('/').pop();
+/* eslint-disable no-magic-numbers */
+const appName = folderName.replace(/[^a-zA-Z0-9_]/, ' ').
+  split(' ').
+  map((w) => `${w[0].toUpperCase()}${w.slice(1)}`).
+  join(' ');
+
 module.exports = Generator.extend({
   writing() {
     this.fs.copy(
@@ -13,13 +20,21 @@ module.exports = Generator.extend({
       this.templatePath('cibuild'),
       this.destinationPath('cibuild')
     );
+    this.fs.copyTpl(
+      this.templatePath('README.md'),
+      this.destinationPath('README.md'),
+      {
+        appName,
+      }
+    );
   },
   installingIonicCli() {
-    const folderName = process.cwd().split('/').pop();
     exec('yarn global add ionic@latest cordova', { stdio: 'inherit' });
     exec(`ionic start ${folderName} --v2 --skip-npm -t blank`, { stdio: 'inherit' });
     exec(`cp -r ${folderName}/. .`);
     exec(`rm -rf ${folderName}`);
+    exec('rm -f tslint.json');
+    exec('rm -f .editorconfig');
     exec('yarn', { stdio: 'inherit' });
   },
 });
